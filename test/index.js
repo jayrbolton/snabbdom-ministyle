57c8c3d66786d5fb411e3e4269e3c1cfce3f8034
Jay R Bolton <jayrbolton@gmail.com>
2019-02-24 07:04:05 -0800

    Rudimentary styles

--- test/index.js
+++ test/index.js
@@ -1,22 +1,33 @@
 const test = require('tape')
 const snabbStyle = require('..')
 const h = require('snabbdom/h').default
-const snabbdom = require('snabbdom').default
+const snabbdom = require('snabbdom')
 
 const patch = snabbdom.init([
   snabbStyle
 ])
 
-
-test('basic styles',  t => {
+test('basic styles', t => {
   const p = h('p', {
     css: {
-      'font-weight': 'bold'
+      root: ['font-weight: bold'],
+      ' span': ['font-weight: normal']
     }
   }, 'lorem ipsum')
-  var container = document.createElement('div')
-  const vnode = patch(container, p)
-  console.log(vnode)
-  console.log(vnode.elm)
+  let container = document.createElement('div')
+  let vnode = patch(container, p)
+  t.strictEqual(vnode.elm.firstChild.innerHTML, '[data-sbid="0"] {font-weight: bold}\n[data-sbid="0"] span {font-weight: normal}\n')
+  const p1 = h('p', {
+    css: {root: ['font-weight: bold']}
+  }, 'hi')
+  const p2 = h('p', {
+    css: {root: ['font-weight: bold']}
+  }, 'hi')
+  const div = h('div', [p1, p2])
+  container = document.createElement('div')
+  vnode = patch(container, div)
+  const ids = Array.from(vnode.elm.children)
+    .map(elm => elm.firstChild.innerHTML.slice(12, 13))
+  t.deepEqual(ids, ['1', '2'])
   t.end()
 })
